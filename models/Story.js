8bec0ee083797fe488076dfb4d4d33d50b704c71
Kushika <118838567+Kushika04@users.noreply.github.com>
2026-02-12 21:35:48 +0530

    Update Story.js

--- models/Story.js
+++ models/Story.js
@@ -2,23 +2,10 @@ const mongoose = require("mongoose");
 
 const storySchema = new mongoose.Schema(
   {
-    userId: {
-      type: String,
-      required: true,
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    images: [
-      {
-        type: String,
-      },
-    ],
+    userId: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    images: [{ type: String }], // array of URLs
   },
   { timestamps: true }
 );
